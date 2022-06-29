dfb4ad0508a925464224003819c4f1a45455d117
Nishedh Karki <restricted.karki@gmail.com>
2022-06-29 09:23:20 +0545

    emptied

--- commands/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'server',
-	desc: 'gives server infos',
-	execute(message, args) {
-		message.channel.send(`::${message.guild.name}::\nhas ${message.guild.memberCount} users.`);
-	},
-
-};
